22d2de372ce0c8560575b0213aa1e3eacee6b106
Priyesh  Padmavilasom <ppadmavilasom@vmware.com>
2017-05-09 01:32:55 +0000

    update to use c-rest-engine api changes
    Change-Id: I5f44e1113b88c1fa217e5c008ff434a0ec39f568
    (cherry picked from commit bfd906fa53748aba50179091a846fb1e9fccc1b9)

--- server/includes.h
+++ server/includes.h
@@ -40,7 +40,7 @@
 
 #include <vmdirclient.h>
 #include <vmafdclient.h>
-#include <vmware-rest/vmrest.h>
+#include <c-rest-engine/vmrest.h>
 #include <tdnf/tdnf.h>
 #include <netmgmt/netmgr.h>
 #include <jansson.h>
