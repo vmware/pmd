c5d360d0d01e502504f6134e85613fe5166eba2f
Harish Udaiya Kumar <hudaiyakumar@vmware.com>
2017-08-02 22:40:26 +0000

    Bug 1930053: [PMD] rest server fails to respond properly when payload is chunked.
    Change-Id: Id4e9d694c2c159f07fbd354b66652b3728ce937b

--- server/restutils/restutils.c
+++ server/restutils/restutils.c
@@ -496,6 +496,7 @@ rest_method(
 
     nDataLength = strlen(pszJsonOut);
     sprintf(pszDataLen, "%d", nDataLength);
+    char* tmp = pszJsonOut;
     if(nDataLength < MAX_HTTP_DATA_LEN)
     {
         dwError = VmRESTSetDataLength(ppResponse, pszDataLen);
@@ -514,29 +515,30 @@ rest_method(
     {
         int nChunkLength = 0;
         int nLengthSent = 0;
-
+        unsigned int nBytesWritten = 0;
         dwError = VmRESTSetDataLength(ppResponse, NULL);
         BAIL_ON_PMD_ERROR(dwError);
 
-        while(!done)
+        do        
         {
             nChunkLength = nDataLength > MAX_HTTP_DATA_LEN ?
                                          MAX_HTTP_DATA_LEN : nDataLength;
-            /*printf("nChunkLength = %d, nDataLength = %d\n", nChunkLength, nDataLength);
-            printf("nLengthSent = %d\n", nLengthSent);
-            printf("Length = %ld\n", strlen(pszJsonOut + nLengthSent));*/
-            dwError = VmRESTSetHttpPayload(
-                          pRestHandle,
-                          ppResponse,
-                          pszJsonOut + nLengthSent,
-                          nChunkLength,
-                          &done
-                          );
-            BAIL_ON_PMD_ERROR(dwError);
-            nLengthSent += nChunkLength;
-            nDataLength -= nChunkLength;
+            dwError = VmRESTSetData(
+                            pRestHandle,
+                            ppResponse,
+                            pszJsonOut + nLengthSent,
+                            nChunkLength,
+                            &nBytesWritten
+                               );
+            if (dwError != REST_ENGINE_MORE_IO_REQUIRED)
+            {
+                BAIL_ON_PMD_ERROR(dwError);
+            }
+            nLengthSent += nBytesWritten;
+            nDataLength -= nBytesWritten;
+            nBytesWritten = 0; //reset the value
             usleep(1000);
-        }
+        }while(dwError == REST_ENGINE_MORE_IO_REQUIRED);
     }
 cleanup:
     PMD_SAFE_FREE_MEMORY(pszURI);
