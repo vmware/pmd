be5a048f6169dcf4158e24a9ea2c405f4240d89b
Tapas Kundu <tkundu@vmware.com>
2019-03-17 15:02:06 +0000

    pmd: compatible wih netmgmt 1.2.0
    rename DNS_MODE_INVALID with DNS_MODE_UNKNOWN
    
    Change-Id: I957d14afe0bce9de47ad9328dfb7cfe577d21bc2

--- tools/cli/netmgmt/net_main.c
+++ tools/cli/netmgmt/net_main.c
@@ -677,7 +677,7 @@ cmd_dns_servers(
 {
     uint32_t dwError = 0, add = 0;
     size_t dwCount = 0, i = 0;
-    NET_DNS_MODE dnsMode = DNS_MODE_INVALID;
+    NET_DNS_MODE dnsMode = DNS_MODE_UNKNOWN;
     char *pszIfname = NULL, *pszMode = NULL;
     char *pszDnsServers = NULL, *pszNoRestart = NULL;
     char *s1, *s2, *pszServers = NULL, **ppszDnsServersList = NULL;
